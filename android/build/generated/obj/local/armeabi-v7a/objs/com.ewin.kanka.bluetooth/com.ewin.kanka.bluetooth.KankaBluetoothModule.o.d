b3fc2c8cfeb24b8377bd0218036b5360a4a35df4
Pablo Lluch <pablo.lluch@gmail.com>
2015-06-18 14:21:37 -0300

    Android files

---
+++ android/build/generated/obj/local/armeabi-v7a/objs/com.ewin.kanka.bluetooth/com.ewin.kanka.bluetooth.KankaBluetoothModule.o.d
@@ -0,0 +1,188 @@
+/tmp/pablo/kanka-bluetooth-generated/obj/local/armeabi-v7a/objs/com.ewin.kanka.bluetooth/com.ewin.kanka.bluetooth.KankaBluetoothModule.o: \
+ jni/com.ewin.kanka.bluetooth.KankaBluetoothModule.cpp \
+ jni/com.ewin.kanka.bluetooth.KankaBluetoothModule.h \
+ /opt/android-ndk/platforms/android-3/arch-arm/usr/include/jni.h \
+ /opt/android-ndk/platforms/android-3/arch-arm/usr/include/sys/cdefs.h \
+ /opt/android-ndk/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h \
+ /opt/android-ndk/platforms/android-3/arch-arm/usr/include/android/api-level.h \
+ /opt/android-ndk/sources/cxx-stl/stlport/stlport/stdarg.h \
+ /opt/android-ndk/sources/cxx-stl/stlport/stlport/stl/_cprolog.h \
+ /opt/android-ndk/sources/cxx-stl/stlport/stlport/stl/config/features.h \
+ /opt/android-ndk/sources/cxx-stl/stlport/stlport/stl/_stlport_version.h \
+ /opt/android-ndk/sources/cxx-stl/stlport/stlport/stl/config/user_config.h \
+ /opt/android-ndk/sources/cxx-stl/stlport/stlport/stl/config/compat.h \
+ /opt/android-ndk/sources/cxx-stl/stlport/stlport/stl/config/host.h \
+ /opt/android-ndk/sources/cxx-stl/stlport/stlport/stl/config/_system.h \
+ /opt/android-ndk/sources/cxx-stl/stlport/stlport/stl/config/_android.h \
+ /opt/android-ndk/sources/cxx-stl/stlport/stlport/stl/config/_gcc.h \
+ /opt/android-ndk/sources/cxx-stl/stlport/stlport/stl/config/stl_confix.h \
+ /opt/android-ndk/sources/cxx-stl/stlport/stlport/stl/config/_native_headers.h \
+ /opt/android-ndk/sources/cxx-stl/stlport/stlport/stl/_epilog.h \
+ /opt/android-ndk/sources/cxx-stl/stlport/stlport/stl/_config_compat_post.h \
+ /home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/Proxy.h \
+ /home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/v8.h \
+ /home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/v8stdint.h \
+ /opt/android-ndk/sources/cxx-stl/stlport/stlport/stddef.h \
+ /opt/android-ndk/sources/cxx-stl/stlport/stlport/stdio.h \
+ /opt/android-ndk/platforms/android-3/arch-arm/usr/include/../include/stdio.h \
+ /opt/android-ndk/platforms/android-3/arch-arm/usr/include/sys/types.h \
+ /opt/android-ndk/platforms/android-3/arch-arm/usr/include/stdint.h \
+ /opt/android-ndk/platforms/android-3/arch-arm/usr/include/sys/_types.h \
+ /opt/android-ndk/platforms/android-3/arch-arm/usr/include/machine/_types.h \
+ /opt/android-ndk/platforms/android-3/arch-arm/usr/include/linux/posix_types.h \
+ /opt/android-ndk/platforms/android-3/arch-arm/usr/include/linux/stddef.h \
+ /opt/android-ndk/platforms/android-3/arch-arm/usr/include/linux/compiler.h \
+ /opt/android-ndk/platforms/android-3/arch-arm/usr/include/asm/posix_types.h \
+ /opt/android-ndk/platforms/android-3/arch-arm/usr/include/asm/types.h \
+ /opt/android-ndk/platforms/android-3/arch-arm/usr/include/linux/types.h \
+ /opt/android-ndk/platforms/android-3/arch-arm/usr/include/machine/kernel.h \
+ /opt/android-ndk/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h \
+ /home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/JavaObject.h \
+ /opt/android-ndk/sources/cxx-stl/stlport/stlport/assert.h \
+ /opt/android-ndk/platforms/android-3/arch-arm/usr/include/../include/assert.h \
+ /home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/EventEmitter.h \
+ /home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/NativeObject.h \
+ /home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/AndroidUtil.h \
+ /opt/android-ndk/platforms/android-3/arch-arm/usr/include/android/log.h \
+ /opt/android-ndk/sources/cxx-stl/stlport/stlport/stdlib.h \
+ /opt/android-ndk/platforms/android-3/arch-arm/usr/include/../include/stdlib.h \
+ /opt/android-ndk/sources/cxx-stl/stlport/stlport/string.h \
+ /opt/android-ndk/platforms/android-3/arch-arm/usr/include/../include/string.h \
+ /opt/android-ndk/platforms/android-3/arch-arm/usr/include/malloc.h \
+ /opt/android-ndk/platforms/android-3/arch-arm/usr/include/alloca.h \
+ /opt/android-ndk/platforms/android-3/arch-arm/usr/include/strings.h \
+ /opt/android-ndk/platforms/android-3/arch-arm/usr/include/memory.h \
+ /home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/EventEmitter.h \
+ /home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/JNIUtil.h \
+ /home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/AndroidUtil.h \
+ /home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/JSException.h \
+ /home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/ProxyFactory.h \
+ /home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/TypeConverter.h \
+ /home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/V8Util.h \
+ jni/com.ewin.kanka.bluetooth.ExampleProxy.h \
+ /home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/org.appcelerator.kroll.KrollModule.h \
+ /home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/Proxy.h
+
+jni/com.ewin.kanka.bluetooth.KankaBluetoothModule.h:
+
+/opt/android-ndk/platforms/android-3/arch-arm/usr/include/jni.h:
+
+/opt/android-ndk/platforms/android-3/arch-arm/usr/include/sys/cdefs.h:
+
+/opt/android-ndk/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/opt/android-ndk/platforms/android-3/arch-arm/usr/include/android/api-level.h:
+
+/opt/android-ndk/sources/cxx-stl/stlport/stlport/stdarg.h:
+
+/opt/android-ndk/sources/cxx-stl/stlport/stlport/stl/_cprolog.h:
+
+/opt/android-ndk/sources/cxx-stl/stlport/stlport/stl/config/features.h:
+
+/opt/android-ndk/sources/cxx-stl/stlport/stlport/stl/_stlport_version.h:
+
+/opt/android-ndk/sources/cxx-stl/stlport/stlport/stl/config/user_config.h:
+
+/opt/android-ndk/sources/cxx-stl/stlport/stlport/stl/config/compat.h:
+
+/opt/android-ndk/sources/cxx-stl/stlport/stlport/stl/config/host.h:
+
+/opt/android-ndk/sources/cxx-stl/stlport/stlport/stl/config/_system.h:
+
+/opt/android-ndk/sources/cxx-stl/stlport/stlport/stl/config/_android.h:
+
+/opt/android-ndk/sources/cxx-stl/stlport/stlport/stl/config/_gcc.h:
+
+/opt/android-ndk/sources/cxx-stl/stlport/stlport/stl/config/stl_confix.h:
+
+/opt/android-ndk/sources/cxx-stl/stlport/stlport/stl/config/_native_headers.h:
+
+/opt/android-ndk/sources/cxx-stl/stlport/stlport/stl/_epilog.h:
+
+/opt/android-ndk/sources/cxx-stl/stlport/stlport/stl/_config_compat_post.h:
+
+/home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/Proxy.h:
+
+/home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/v8.h:
+
+/home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/v8stdint.h:
+
+/opt/android-ndk/sources/cxx-stl/stlport/stlport/stddef.h:
+
+/opt/android-ndk/sources/cxx-stl/stlport/stlport/stdio.h:
+
+/opt/android-ndk/platforms/android-3/arch-arm/usr/include/../include/stdio.h:
+
+/opt/android-ndk/platforms/android-3/arch-arm/usr/include/sys/types.h:
+
+/opt/android-ndk/platforms/android-3/arch-arm/usr/include/stdint.h:
+
+/opt/android-ndk/platforms/android-3/arch-arm/usr/include/sys/_types.h:
+
+/opt/android-ndk/platforms/android-3/arch-arm/usr/include/machine/_types.h:
+
+/opt/android-ndk/platforms/android-3/arch-arm/usr/include/linux/posix_types.h:
+
+/opt/android-ndk/platforms/android-3/arch-arm/usr/include/linux/stddef.h:
+
+/opt/android-ndk/platforms/android-3/arch-arm/usr/include/linux/compiler.h:
+
+/opt/android-ndk/platforms/android-3/arch-arm/usr/include/asm/posix_types.h:
+
+/opt/android-ndk/platforms/android-3/arch-arm/usr/include/asm/types.h:
+
+/opt/android-ndk/platforms/android-3/arch-arm/usr/include/linux/types.h:
+
+/opt/android-ndk/platforms/android-3/arch-arm/usr/include/machine/kernel.h:
+
+/opt/android-ndk/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h:
+
+/home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/JavaObject.h:
+
+/opt/android-ndk/sources/cxx-stl/stlport/stlport/assert.h:
+
+/opt/android-ndk/platforms/android-3/arch-arm/usr/include/../include/assert.h:
+
+/home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/EventEmitter.h:
+
+/home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/NativeObject.h:
+
+/home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/AndroidUtil.h:
+
+/opt/android-ndk/platforms/android-3/arch-arm/usr/include/android/log.h:
+
+/opt/android-ndk/sources/cxx-stl/stlport/stlport/stdlib.h:
+
+/opt/android-ndk/platforms/android-3/arch-arm/usr/include/../include/stdlib.h:
+
+/opt/android-ndk/sources/cxx-stl/stlport/stlport/string.h:
+
+/opt/android-ndk/platforms/android-3/arch-arm/usr/include/../include/string.h:
+
+/opt/android-ndk/platforms/android-3/arch-arm/usr/include/malloc.h:
+
+/opt/android-ndk/platforms/android-3/arch-arm/usr/include/alloca.h:
+
+/opt/android-ndk/platforms/android-3/arch-arm/usr/include/strings.h:
+
+/opt/android-ndk/platforms/android-3/arch-arm/usr/include/memory.h:
+
+/home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/EventEmitter.h:
+
+/home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/JNIUtil.h:
+
+/home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/AndroidUtil.h:
+
+/home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/JSException.h:
+
+/home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/ProxyFactory.h:
+
+/home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/TypeConverter.h:
+
+/home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/V8Util.h:
+
+jni/com.ewin.kanka.bluetooth.ExampleProxy.h:
+
+/home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/org.appcelerator.kroll.KrollModule.h:
+
+/home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/Proxy.h:
---
+++ android/build/generated/obj/local/x86/objs/com.ewin.kanka.bluetooth/com.ewin.kanka.bluetooth.KankaBluetoothModule.o.d
@@ -0,0 +1,191 @@
+/tmp/pablo/kanka-bluetooth-generated/obj/local/x86/objs/com.ewin.kanka.bluetooth/com.ewin.kanka.bluetooth.KankaBluetoothModule.o: \
+ jni/com.ewin.kanka.bluetooth.KankaBluetoothModule.cpp \
+ jni/com.ewin.kanka.bluetooth.KankaBluetoothModule.h \
+ /opt/android-ndk/platforms/android-9/arch-x86/usr/include/jni.h \
+ /opt/android-ndk/platforms/android-9/arch-x86/usr/include/sys/cdefs.h \
+ /opt/android-ndk/platforms/android-9/arch-x86/usr/include/sys/cdefs_elf.h \
+ /opt/android-ndk/platforms/android-9/arch-x86/usr/include/android/api-level.h \
+ /opt/android-ndk/sources/cxx-stl/stlport/stlport/stdarg.h \
+ /opt/android-ndk/sources/cxx-stl/stlport/stlport/stl/_cprolog.h \
+ /opt/android-ndk/sources/cxx-stl/stlport/stlport/stl/config/features.h \
+ /opt/android-ndk/sources/cxx-stl/stlport/stlport/stl/_stlport_version.h \
+ /opt/android-ndk/sources/cxx-stl/stlport/stlport/stl/config/user_config.h \
+ /opt/android-ndk/sources/cxx-stl/stlport/stlport/stl/config/compat.h \
+ /opt/android-ndk/sources/cxx-stl/stlport/stlport/stl/config/host.h \
+ /opt/android-ndk/sources/cxx-stl/stlport/stlport/stl/config/_system.h \
+ /opt/android-ndk/sources/cxx-stl/stlport/stlport/stl/config/_android.h \
+ /opt/android-ndk/sources/cxx-stl/stlport/stlport/stl/config/_gcc.h \
+ /opt/android-ndk/sources/cxx-stl/stlport/stlport/stl/config/stl_confix.h \
+ /opt/android-ndk/sources/cxx-stl/stlport/stlport/stl/config/_native_headers.h \
+ /opt/android-ndk/sources/cxx-stl/stlport/stlport/stl/_epilog.h \
+ /opt/android-ndk/sources/cxx-stl/stlport/stlport/stl/_config_compat_post.h \
+ /home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/Proxy.h \
+ /home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/v8.h \
+ /home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/v8stdint.h \
+ /opt/android-ndk/sources/cxx-stl/stlport/stlport/stddef.h \
+ /opt/android-ndk/sources/cxx-stl/stlport/stlport/stdio.h \
+ /opt/android-ndk/platforms/android-9/arch-x86/usr/include/../include/stdio.h \
+ /opt/android-ndk/platforms/android-9/arch-x86/usr/include/sys/types.h \
+ /opt/android-ndk/platforms/android-9/arch-x86/usr/include/stdint.h \
+ /opt/android-ndk/platforms/android-9/arch-x86/usr/include/sys/_types.h \
+ /opt/android-ndk/platforms/android-9/arch-x86/usr/include/machine/_types.h \
+ /opt/android-ndk/platforms/android-9/arch-x86/usr/include/linux/posix_types.h \
+ /opt/android-ndk/platforms/android-9/arch-x86/usr/include/linux/stddef.h \
+ /opt/android-ndk/platforms/android-9/arch-x86/usr/include/linux/compiler.h \
+ /opt/android-ndk/platforms/android-9/arch-x86/usr/include/asm/posix_types.h \
+ /opt/android-ndk/platforms/android-9/arch-x86/usr/include/asm/posix_types_32.h \
+ /opt/android-ndk/platforms/android-9/arch-x86/usr/include/asm/types.h \
+ /opt/android-ndk/platforms/android-9/arch-x86/usr/include/linux/types.h \
+ /opt/android-ndk/platforms/android-9/arch-x86/usr/include/machine/kernel.h \
+ /opt/android-ndk/platforms/android-9/arch-x86/usr/include/sys/sysmacros.h \
+ /home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/JavaObject.h \
+ /opt/android-ndk/sources/cxx-stl/stlport/stlport/assert.h \
+ /opt/android-ndk/platforms/android-9/arch-x86/usr/include/../include/assert.h \
+ /home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/EventEmitter.h \
+ /home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/NativeObject.h \
+ /home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/AndroidUtil.h \
+ /opt/android-ndk/platforms/android-9/arch-x86/usr/include/android/log.h \
+ /opt/android-ndk/sources/cxx-stl/stlport/stlport/stdlib.h \
+ /opt/android-ndk/platforms/android-9/arch-x86/usr/include/../include/stdlib.h \
+ /opt/android-ndk/sources/cxx-stl/stlport/stlport/string.h \
+ /opt/android-ndk/platforms/android-9/arch-x86/usr/include/../include/string.h \
+ /opt/android-ndk/platforms/android-9/arch-x86/usr/include/malloc.h \
+ /opt/android-ndk/platforms/android-9/arch-x86/usr/include/alloca.h \
+ /opt/android-ndk/platforms/android-9/arch-x86/usr/include/strings.h \
+ /opt/android-ndk/platforms/android-9/arch-x86/usr/include/memory.h \
+ /home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/EventEmitter.h \
+ /home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/JNIUtil.h \
+ /home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/AndroidUtil.h \
+ /home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/JSException.h \
+ /home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/ProxyFactory.h \
+ /home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/TypeConverter.h \
+ /home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/V8Util.h \
+ jni/com.ewin.kanka.bluetooth.ExampleProxy.h \
+ /home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/org.appcelerator.kroll.KrollModule.h \
+ /home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/Proxy.h
+
+jni/com.ewin.kanka.bluetooth.KankaBluetoothModule.h:
+
+/opt/android-ndk/platforms/android-9/arch-x86/usr/include/jni.h:
+
+/opt/android-ndk/platforms/android-9/arch-x86/usr/include/sys/cdefs.h:
+
+/opt/android-ndk/platforms/android-9/arch-x86/usr/include/sys/cdefs_elf.h:
+
+/opt/android-ndk/platforms/android-9/arch-x86/usr/include/android/api-level.h:
+
+/opt/android-ndk/sources/cxx-stl/stlport/stlport/stdarg.h:
+
+/opt/android-ndk/sources/cxx-stl/stlport/stlport/stl/_cprolog.h:
+
+/opt/android-ndk/sources/cxx-stl/stlport/stlport/stl/config/features.h:
+
+/opt/android-ndk/sources/cxx-stl/stlport/stlport/stl/_stlport_version.h:
+
+/opt/android-ndk/sources/cxx-stl/stlport/stlport/stl/config/user_config.h:
+
+/opt/android-ndk/sources/cxx-stl/stlport/stlport/stl/config/compat.h:
+
+/opt/android-ndk/sources/cxx-stl/stlport/stlport/stl/config/host.h:
+
+/opt/android-ndk/sources/cxx-stl/stlport/stlport/stl/config/_system.h:
+
+/opt/android-ndk/sources/cxx-stl/stlport/stlport/stl/config/_android.h:
+
+/opt/android-ndk/sources/cxx-stl/stlport/stlport/stl/config/_gcc.h:
+
+/opt/android-ndk/sources/cxx-stl/stlport/stlport/stl/config/stl_confix.h:
+
+/opt/android-ndk/sources/cxx-stl/stlport/stlport/stl/config/_native_headers.h:
+
+/opt/android-ndk/sources/cxx-stl/stlport/stlport/stl/_epilog.h:
+
+/opt/android-ndk/sources/cxx-stl/stlport/stlport/stl/_config_compat_post.h:
+
+/home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/Proxy.h:
+
+/home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/v8.h:
+
+/home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/v8stdint.h:
+
+/opt/android-ndk/sources/cxx-stl/stlport/stlport/stddef.h:
+
+/opt/android-ndk/sources/cxx-stl/stlport/stlport/stdio.h:
+
+/opt/android-ndk/platforms/android-9/arch-x86/usr/include/../include/stdio.h:
+
+/opt/android-ndk/platforms/android-9/arch-x86/usr/include/sys/types.h:
+
+/opt/android-ndk/platforms/android-9/arch-x86/usr/include/stdint.h:
+
+/opt/android-ndk/platforms/android-9/arch-x86/usr/include/sys/_types.h:
+
+/opt/android-ndk/platforms/android-9/arch-x86/usr/include/machine/_types.h:
+
+/opt/android-ndk/platforms/android-9/arch-x86/usr/include/linux/posix_types.h:
+
+/opt/android-ndk/platforms/android-9/arch-x86/usr/include/linux/stddef.h:
+
+/opt/android-ndk/platforms/android-9/arch-x86/usr/include/linux/compiler.h:
+
+/opt/android-ndk/platforms/android-9/arch-x86/usr/include/asm/posix_types.h:
+
+/opt/android-ndk/platforms/android-9/arch-x86/usr/include/asm/posix_types_32.h:
+
+/opt/android-ndk/platforms/android-9/arch-x86/usr/include/asm/types.h:
+
+/opt/android-ndk/platforms/android-9/arch-x86/usr/include/linux/types.h:
+
+/opt/android-ndk/platforms/android-9/arch-x86/usr/include/machine/kernel.h:
+
+/opt/android-ndk/platforms/android-9/arch-x86/usr/include/sys/sysmacros.h:
+
+/home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/JavaObject.h:
+
+/opt/android-ndk/sources/cxx-stl/stlport/stlport/assert.h:
+
+/opt/android-ndk/platforms/android-9/arch-x86/usr/include/../include/assert.h:
+
+/home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/EventEmitter.h:
+
+/home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/NativeObject.h:
+
+/home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/AndroidUtil.h:
+
+/opt/android-ndk/platforms/android-9/arch-x86/usr/include/android/log.h:
+
+/opt/android-ndk/sources/cxx-stl/stlport/stlport/stdlib.h:
+
+/opt/android-ndk/platforms/android-9/arch-x86/usr/include/../include/stdlib.h:
+
+/opt/android-ndk/sources/cxx-stl/stlport/stlport/string.h:
+
+/opt/android-ndk/platforms/android-9/arch-x86/usr/include/../include/string.h:
+
+/opt/android-ndk/platforms/android-9/arch-x86/usr/include/malloc.h:
+
+/opt/android-ndk/platforms/android-9/arch-x86/usr/include/alloca.h:
+
+/opt/android-ndk/platforms/android-9/arch-x86/usr/include/strings.h:
+
+/opt/android-ndk/platforms/android-9/arch-x86/usr/include/memory.h:
+
+/home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/EventEmitter.h:
+
+/home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/JNIUtil.h:
+
+/home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/AndroidUtil.h:
+
+/home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/JSException.h:
+
+/home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/ProxyFactory.h:
+
+/home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/TypeConverter.h:
+
+/home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/V8Util.h:
+
+jni/com.ewin.kanka.bluetooth.ExampleProxy.h:
+
+/home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/org.appcelerator.kroll.KrollModule.h:
+
+/home/pablo/.titanium/mobilesdk/linux/4.1.0.v20150610125228/android/native/include/Proxy.h:
